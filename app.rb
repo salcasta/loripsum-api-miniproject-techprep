d8a96f996af2c181a5969083d9887cac769c8f87
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2023-08-05 03:46:39 +0000

    links and text boxes are working

--- app.rb
+++ app.rb
@@ -7,3 +7,21 @@ get("/") do
   @output = HTTP.get(@url)
 erb(:main_page)
 end
+
+
+get("/results") do
+
+  paragraph = params.fetch("paragraph")
+  paragraph_append = "/#{paragraph}"
+
+  length = params.fetch("length")
+  length_append = "/#{length}"
+
+
+  
+
+  @url = "https://www.loripsum.net/api#{paragraph_append}#{length_append}" 
+  @output = HTTP.get(@url)
+ 
+erb(:result_page)
+end
